24747829e4667d87e69de23e8a8f4bd3603aaaa9
ZzzHe <x121144@163.com>
2026-09-01 10:51:56 +0800

    chore: set fallback charge UP to 18461303

--- src/Ray.BiliBiliTool.Config/Constants.cs
+++ src/Ray.BiliBiliTool.Config/Constants.cs
@@ -17,7 +17,7 @@ public static class Constants
     /// </summary>
     public static string SourceCodeUrl = "https://github.com/RayWangQvQ/BiliBiliToolPro";
 
-    public static string FallbackAutoChargeUpId = "220893216";
+    public static string FallbackAutoChargeUpId = "18461303";
 
     /// <summary>
     /// 每日任务exp
